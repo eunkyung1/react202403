9037ef867d25726697313c4780dc1bbc1078879b
EK <eunkyung2625@daum.net>
2024-03-29 22:48:10 +0900

    3/29

--- r0329/src/App.js
+++ r0329/src/App.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [ 파이, d] = useState('파이썬독학');
+  let [logo,setLogo] = useState('ReactBlog'); 
+
+
+  let num = [1,2];
+  //let [a,c] = [1,2];
   
+  //let a = num[0];
+  //let c = num[1];
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color:'red',fontSize:'16px'}}>블로그임</h4>
+        <h4 style={{color:'red',fontSize:'16px'}}>{logo}</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{글제목}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{강남}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{파이}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      
     </div>
   );
 }
