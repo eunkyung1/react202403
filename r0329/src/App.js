0841efeb94c0c74fd3824ab6853ec68a4d259009
EK <eunkyung2625@daum.net>
2024-03-29 21:40:53 +0900

    3/29

--- r0329/src/App.js
+++ r0329/src/App.js
@@ -1,27 +1,17 @@
 import React from "react";
-import {Routes, Route, Link} from "react-router-dom";
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Counter from "./pages/Counter"
-import Input from "./pages/Input"
-import Input2 from "./pages/Input2"
-import List from "./pages/List"
+import './App.css';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  
+
   return (
     <div className="App">
-      <nav>
-      <Link to="/">Home</Link>  |  <Link to="/about">About</Link>  |  <Link to="/counter">Counter</Link>  |  <Link to="/input">Input</Link>  |  <Link to="/input2">Input2</Link> |  <Link to="/list">List</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/counter" element={<Counter/>} />
-        <Route path="/input" element={<Input/>} />
-        <Route path="/input2" element={<Input2/>} />
-        <Route path="/list" element={<List/>} />
-     </Routes>
- 
+      <div className="black-nav">
+        <h4 style={{color:'red',fontSize:'16px'}}>블로그임</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
